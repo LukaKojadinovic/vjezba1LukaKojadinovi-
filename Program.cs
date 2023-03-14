f2c20b7af3421c795189c39906fa2d517b3f3f97
RSpral <rspral00@fesb.hr>
2023-03-14 17:22:27 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vjezba1LukaKojadinović
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
